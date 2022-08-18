3ebe31ea56b20daf4c52359a5ea81125c3c162e8
Quang Huy <ngoquanghuy2210@gmail.com>
2022-08-18 08:08:12 +0700

    he

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,7 +1,8 @@
+import React from 'react';
+import Tippy from '@tippyjs/react/headless';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
-
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 const cx = classNames.bind(styles);
@@ -13,19 +14,26 @@ function Header() {
         <div className={cx('logo')}>
           <img src={images.logo} alt="TikTok" />
         </div>
-        <div className={cx('search')}>
-          <input placeholder="Search accounts and videos" spellCheck={false} />
-          <button className={cx('clear')}>
-            <FontAwesomeIcon icon={faCircleXmark} />
-          </button>
-
-          <button className={cx('loading')}>
-            <FontAwesomeIcon icon={faSpinner} />
-          </button>
-          <button className={cx('search-icon')}>
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
-          </button>
-        </div>
+        <Tippy
+          render={(attrs) => (
+            <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+              Kết quả
+            </div>
+          )}
+        >
+          <div className={cx('search')}>
+            <input placeholder="Search accounts and videos" spellCheck={false} />
+            <button className={cx('clear')}>
+              <FontAwesomeIcon icon={faCircleXmark} />
+            </button>
+            <button className={cx('loading')}>
+              <FontAwesomeIcon icon={faSpinner} />
+            </button>
+            <button className={cx('search-icon')}>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+          </div>
+        </Tippy>
         <div className={cx('action')}></div>
       </div>
     </header>
