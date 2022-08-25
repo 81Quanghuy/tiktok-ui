215b1e14845e4a48d72dcecc83e767b44ebf45fa
Quang Huy <ngoquanghuy2210@gmail.com>
2022-08-25 21:11:21 +0700

    update 25/8

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,26 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+  return (
+    <div className={cx('wrapper')}>
+      <img
+        className={cx('avatar')}
+        src="https://scontent.fsgn13-4.fna.fbcdn.net/v/t39.30808-6/298592587_1735332750158769_5954008647144630258_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=FCGzo2ZinesAX-jbdxi&_nc_ht=scontent.fsgn13-4.fna&oh=00_AT-HouQhJDz6KicSQYOh2OAWuPtGw-J9XdGR6EBYqV8cbQ&oe=6306FFA6"
+        alt="avatar"
+      />
+      <div className={cx('info')}>
+        <p className={cx('name')}>
+          <h4>Nguyen Van A</h4>
+          <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+        </p>
+        <span className={cx('username')}>nguyenvana</span>
+      </div>
+    </div>
+  );
+}
+
+export default AccountItem;
--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,39 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Tippy from '@tippyjs/react/headless';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+
+import { Wrapper as PopperWrapper } from '~/components/Popper';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
+import AccountItem from '~/components/AccountItem';
+
 const cx = classNames.bind(styles);
 
 function Header() {
+  const [searchResult, setSearchResult] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSearchResult([]);
+    }, 0);
+  });
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
+        {/* logo */}
         <div className={cx('logo')}>
           <img src={images.logo} alt="TikTok" />
         </div>
+        {/* Search */}
         <Tippy
+          interactive
+          visible={searchResult.length > 0}
           render={(attrs) => (
             <div className={cx('search-result')} tabIndex="-1" {...attrs}>
-              Kết quả
+              <PopperWrapper>
+                <h4 className={cx('search-label')}>Accounts</h4>
+                <AccountItem />
+                <AccountItem />
+                <AccountItem />
+                <AccountItem />
+              </PopperWrapper>
             </div>
           )}
         >
           <div className={cx('search')}>
             <input placeholder="Search accounts and videos" spellCheck={false} />
+            {/* Button Clear */}
             <button className={cx('clear')}>
               <FontAwesomeIcon icon={faCircleXmark} />
             </button>
+            {/* Button loading */}
             <button className={cx('loading')}>
               <FontAwesomeIcon icon={faSpinner} />
             </button>
+            {/* Button search */}
             <button className={cx('search-icon')}>
               <FontAwesomeIcon icon={faMagnifyingGlass} />
             </button>
           </div>
         </Tippy>
+        {/* action */}
         <div className={cx('action')}></div>
       </div>
     </header>
